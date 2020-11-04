847546898ff64ecc7aab0ecd23f74e2f2b394a9d
M Faris Gibran <mfarisgibran@gmail.com>
2020-11-04 22:41:56 +0700

    Display data to DOM

--- index.js
+++ index.js
@@ -96,3 +96,10 @@ const properties = [
 ]
 
 const propertyListElement = document.getElementById('property-list')
+
+properties.forEach((property) => {
+  const propertyElement = document.createElement('div')
+  propertyElement.innerHTML = `<h1>${property.name}</h1>
+  <h2>${property.price}</h2>`
+  propertyListElement.appendChild(propertyElement)
+})
