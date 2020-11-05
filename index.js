0fef37443b1ed50e22d21006586e9400f1f03a98
M Faris Gibran <mfarisgibran@gmail.com>
2020-11-05 20:52:34 +0700

    Display location of properties

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ const formatRupiah = (price) => {
   }).format(price)
 }
 
+const formatLocation = (location) => {
+  return `${location.address}, ${location.city}, ${location.province}, ${location.zipCode}`
+}
+
 properties.forEach((property) => {
   const propertyElement = document.createElement('div')
   propertyElement.innerHTML = `
+  <img src='/images/mfarisgibran-house.webp' height='100'/>
   <h1>${property.name}</h1>
   <h2>${property.type} ${property.subtype}</h2>
-  <h3>${formatRupiah(property.price)}</h3> 
-  <img src='/images/mfarisgibran-house.webp' height='100'>
+  <h3>${formatRupiah(property.price)}</h3>
+  <p>${property.numberOfBedrooms} bd ${property.numberOfBathrooms} ba ${
+    property.unitSize
+  } m²</p>
+  <p>${formatLocation(property.location)}</p>
+  <hr/>
   `
   propertyListElement.appendChild(propertyElement)
 })
