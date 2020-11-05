37cb351aa583a2725c098c2778386c0607d401c4
M Faris Gibran <mfarisgibran@gmail.com>
2020-11-05 20:33:05 +0700

    Display price in Rupiah

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ import { properties } from './properties.js'
 
 const propertyListElement = document.getElementById('property-list')
 
-console.log(properties[0])
+const formatRupiah = (price) => {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+  }).format(price)
+}
 
 properties.forEach((property) => {
   const propertyElement = document.createElement('div')
   propertyElement.innerHTML = `
   <h1>${property.name}</h1>
-  <h2>${property.price}</h2> 
+  <h2>${formatRupiah(property.price)}</h2> 
   <img src='/images/mfarisgibran-house.webp' height='100'>
   `
   propertyListElement.appendChild(propertyElement)
