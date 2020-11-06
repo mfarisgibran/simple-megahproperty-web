86d1b5f1e7db2f43bdb99680203070df6ca8f85a
M Faris Gibran <mfarisgibran@gmail.com>
2020-11-06 22:14:31 +0700

    Import export function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { properties } from './properties.js'
 
 const propertyListElement = document.getElementById('property-list')
 
-const formatRupiah = (price) => {
+export const formatRupiah = (price) => {
   return new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR',
@@ -31,27 +31,34 @@ const formatLocation = (location) => {
   return `${location.address}, ${location.city}, ${location.province}, ${location.zipCode}`
 }
 
-properties.forEach((property) => {
-  const propertyElement = document.createElement('a')
-  propertyElement.classList.add('property')
-  propertyElement.setAttribute('href', `/property/index.html?id=${property.id}`)
+const displayProperties = () => {
+  properties.forEach((property) => {
+    const propertyElement = document.createElement('a')
+    propertyElement.classList.add('property')
+    propertyElement.setAttribute(
+      'href',
+      `/property/index.html?id=${property.id}`
+    )
 
-  propertyElement.innerHTML = `
+    propertyElement.innerHTML = `
     <img class="property-image" src="${
       property.images[0]
     }" height="250" width="350"/>
     <div class="property-info">
-      <h1 class="property-name">${property.name}</h1>
-      <h2 class="property-price">${formatRupiah(property.price)}</h2>
-      <h3>${property.type} ${property.subtype}</h3>
-      <p>
-      <span>${formatBedroomText(property.numberOfBedrooms)} |</span>
-      <span>${formatBathroomText(property.numberOfBathrooms)} |</span>
-      <span>${property.unitSize} m²</span>
-      </p>
-      <p>${formatLocation(property.location)}</p>
+    <h1 class="property-name">${property.name}</h1>
+    <h2 class="property-price">${formatRupiah(property.price)}</h2>
+    <h3>${property.type} ${property.subtype}</h3>
+    <p>
+    <span>${formatBedroomText(property.numberOfBedrooms)} |</span>
+    <span>${formatBathroomText(property.numberOfBathrooms)} |</span>
+    <span>${property.unitSize} m²</span>
+    </p>
+    <p>${formatLocation(property.location)}</p>
     </div>
-  `
+    `
 
-  propertyListElement.appendChild(propertyElement)
-})
+    propertyListElement.appendChild(propertyElement)
+  })
+}
+
+displayProperties()
--- property/index.js
+++ property/index.js
@@ -1,1 +1,15 @@
-console.log('Ini adalah halaman property')
+import { properties } from '../properties.js'
+import { formatRupiah } from '../index.js'
+
+const query = new URLSearchParams(window.location.search)
+const id = Number(query.get('id'))
+
+const selectedProperty = properties.find((property) => {
+  if (id === property.id) {
+    return property
+  }
+})
+
+const propertyPrice = document.getElementById('property-price')
+
+propertyPrice.innerText = selectedProperty.price
