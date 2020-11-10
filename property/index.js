a8f024bbae8fcb3d5f4e617b301e3651653c452a
M Faris Gibran <mfarisgibran@gmail.com>
2020-11-10 13:24:12 +0700

    Display more information on individual property

--- property/index.js
+++ property/index.js
@@ -15,8 +15,41 @@ const selectedProperty = properties.find((property) => {
   }
 })
 
+/*const propertyImage = document.getElementById('property-image')
+
+selectedProperty.images.forEach((image) => {
+  const imageElement = document.createElement('img')
+  imageElement.innerText = image
+  propertyImage.appendChild(imageElement)
+})*/
+
+const propertyName = document.getElementById('property-name')
+propertyName.innerText = selectedProperty.name
+
 const propertyPrice = document.getElementById('property-price')
 propertyPrice.innerText = formatRupiah(selectedProperty.price)
+
+/*
+const propertyType = document.getElementById('property-type-subtype')
+propertyType.innerText = selectedProperty.type
+
+const propertySubtype = document.getElementById('property-type-subtype')
+propertySubtype.innerText = selectedProperty.subtype
+*/
+
+const propertyBedroom = document.getElementById('property-bedroom')
+propertyBedroom.innerText = formatBedroomText(selectedProperty.numberOfBedrooms)
+
+const propertyBathroom = document.getElementById('property-bathroom')
+propertyBathroom.innerText = formatBathroomText(
+  selectedProperty.numberOfBathrooms
+)
+
+const propertyUnitSize = document.getElementById('property-unit-size')
+propertyUnitSize.innerText = selectedProperty.unitSize
+
+const propertyLocation = document.getElementById('property-location')
+propertyLocation.innerText = formatLocation(selectedProperty.location)
 
 const propertyNearestObjects = document.getElementById(
   'property-nearest-objects'
